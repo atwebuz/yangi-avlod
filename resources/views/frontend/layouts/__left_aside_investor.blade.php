09e9000bc204331616957bc6f8052667c9a6ddb0
InvestUz <invest.abdusattorov@gmail.com>
2024-07-25 20:14:22 +0500

    aside route active has been fixed

--- resources/views/frontend/layouts/__left_aside_investor.blade.php
+++ resources/views/frontend/layouts/__left_aside_investor.blade.php
@@ -3,15 +3,14 @@
         Инвесторам </div>
 
 
-    <nav class="aside__list">
-        <span class="aside__link active">О нас</span>
-        <a class="aside__link" href="{{route('frontend.investoram_premushastva')}}">Преимущества промышленной зоны</a>
-        <a class="aside__link" href="{{ route('frontend.investoram_registratsiya_rezidentov') }}">Регистрация
-            резидентов</a>
-        <a class="aside__link" href="{{route('frontend.investoram_nalogovie_preferensi')}}">НАЛОГОВЫЕ ПРЕФЕРЕНЦИИ</a>
-        <a class="aside__link" href="{{route('frontend.investoram_podderjka_eksportov')}}">Поддержка экспортёров</a>
-     
-    </nav>
+        <nav class="aside__list">
+            <span class="aside__link {{ request()->routeIs('frontend.about') ? 'active' : '' }}">О нас</span>
+            <a class="aside__link {{ request()->routeIs('frontend.investoram_premushastva') ? 'active' : '' }}" href="{{ route('frontend.investoram_premushastva') }}">Преимущества промышленной зоны</a>
+            <a class="aside__link {{ request()->routeIs('frontend.investoram_registratsiya_rezidentov') ? 'active' : '' }}" href="{{ route('frontend.investoram_registratsiya_rezidentov') }}">Регистрация резидентов</a>
+            <a class="aside__link {{ request()->routeIs('frontend.investoram_nalogovie_preferensi') ? 'active' : '' }}" href="{{ route('frontend.investoram_nalogovie_preferensi') }}">НАЛОГОВЫЕ ПРЕФЕРЕНЦИИ</a>
+            <a class="aside__link {{ request()->routeIs('frontend.investoram_podderjka_eksportov') ? 'active' : '' }}" href="{{ route('frontend.investoram_podderjka_eksportov') }}">Поддержка экспортёров</a>
+        </nav>
+        
 
 
 </aside>
